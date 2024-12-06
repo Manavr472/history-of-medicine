555f186c2e2fecc48005f3bd3c957ec8133ff470
Manavr472 <manavr472@gmail.com>
2024-12-06 17:37:58 +0530

    surgeon info

--- src/app/surgeoninfo/page.tsx
+++ src/app/surgeoninfo/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import { motion, useAnimation } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 const SurgeonInfo: React.FC = () => {
     const surgeons = [
