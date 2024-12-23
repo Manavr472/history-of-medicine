a8929d83495452fdf0fd5ef7ba8ca4015a955007
Manavr472 <manavr472@gmail.com>
2024-12-23 15:59:16 +0530

    XR first draft

--- src/app/xrmuseum/page.tsx
+++ src/app/xrmuseum/page.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import Museum from '../components/museum';
 import { CameraKit } from '../contexts/CameraKitContext';
 
-const Page = () => (
-  <CameraKit>
-    <Museum />
-  </CameraKit>
-);
 
-export default Page;
+const XRmuseum: React.FC = () => {
+    return (
+        <CameraKit>
+            <Museum />
+        </CameraKit>
+    );
+};
+
+export default XRmuseum;
