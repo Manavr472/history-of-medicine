c36e766e549b6ea93f64062542d012800d25a751
Manavr472 <manavr472@gmail.com>
2024-12-23 16:06:49 +0530

    XR first draft

--- src/app/contexts/CameraKitContext.tsx
+++ src/app/contexts/CameraKitContext.tsx
@@ -1,5 +1,5 @@
 import { bootstrapCameraKit, CameraKitSession, Lens } from '@snap/camera-kit';
-import { createContext, useEffect, useRef, useState } from 'react';
+import { createContext, useEffect, useRef, useState, useContext } from 'react';
 
 const apiToken = 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM0OTM1MDg5LCJzdWIiOiIwYzQzMDhhYi1hNGFmLTRhZDQtODBhYS1iOTBmOTZlNjFlNTl-U1RBR0lOR34yODU5YmExOC1lZThmLTRhY2QtODNhMy02Zjg5MWRhNmY0YmUifQ.xIDyPV5eZF1yIpCHcguCySzmnxWvzqHR6M2BgOzX1wI';
 const lensGroupId = 'e5093c15-440e-4bdb-8476-5555060bc1c0';
@@ -19,6 +19,8 @@ export const CameraKit: React.FC<{ children: React.ReactNode }> = ({
   const [lenses, setLenses] = useState<Lens[]>([]);
 
   useEffect(() => {
+    if (typeof window === 'undefined') return; // Ensure this code runs only on the client side
+
     const initializeCameraKit = async () => {
       const cameraKit = await bootstrapCameraKit({ apiToken });
       const session = await cameraKit.createSession();
@@ -36,11 +38,13 @@ export const CameraKit: React.FC<{ children: React.ReactNode }> = ({
     initializeCameraKit();
   }, []);
 
-  return !session ? (
-    <div>Initializing Camera Kit...</div>
-  ) : (
+  return !session ? null : (
     <CameraKitContext.Provider value={{ session, lenses }}>
       {children}
     </CameraKitContext.Provider>
   );
+};
+
+export const useCameraKit = () => {
+  return useContext(CameraKitContext);
 };
