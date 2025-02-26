6aa2956a09de209ebaf01debf9d722e575340b54
Manavr472 <manavr472@gmail.com>
2025-02-26 21:29:27 +0530

    modelar

--- src/app/xrmuseum/page.tsx
+++ src/app/xrmuseum/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-const NewXRClient = dynamic(() => import('../components/newxr'), { ssr: false });
+// const NewXRClient = dynamic(() => import('../components/newxr'), { ssr: false });
 const ModelViewerComponent = dynamic(() => import('../components/modelar'), { ssr: false });
 
 const XRMuseum: React.FC = () => {
