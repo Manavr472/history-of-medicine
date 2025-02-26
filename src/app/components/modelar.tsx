df62d1cad65ec53d093160ec56919525e65b389a
Manavr472 <manavr472@gmail.com>
2025-02-26 21:49:58 +0530

    modelar

--- src/app/components/modelar.tsx
+++ src/app/components/modelar.tsx
@@ -5,7 +5,7 @@ const ModelViewerComponent: React.FC = () => {
     return (
         <>
         <html lang="en">
-        <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js" async />
             <model-viewer src="3d-models/Glucks_First_Model_no_anime.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls tone-mapping="neutral" poster="poster.webp" shadow-intensity="1" style={{ width: '100%', height: '500px' }}>
                     <div className="progress-bar hide" slot="progress-bar">
                         <div className="update-bar"></div>
