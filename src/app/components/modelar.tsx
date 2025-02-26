b7ddf2bafc464205ad3be4e6673db6882ec1c0ba
Manavr472 <manavr472@gmail.com>
2025-02-26 21:46:00 +0530

    modelar

--- src/app/components/modelar.tsx
+++ src/app/components/modelar.tsx
@@ -3,25 +3,20 @@ import '@google/model-viewer';
 
 const ModelViewerComponent: React.FC = () => {
     return (
-        <div>
-            <model-viewer src="3d-models/Glucks_First_Model_no_anime.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1" style={{ width: '100%', height: '500px' }}>
-                <div className="progress-bar hide" slot="progress-bar">
-                    <div className="update-bar"></div>
-                </div>
-                <button slot="ar-button" id="ar-button">
-                    View in your space
-                </button>
+        <>
+        <html lang="en">
+        <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js"></script>
+            <model-viewer src="3d-models/Glucks_First_Model_no_anime.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls tone-mapping="neutral" poster="poster.webp" shadow-intensity="1" style={{ width: '100%', height: '500px' }}>
+                    <div className="progress-bar hide" slot="progress-bar">
+                        <div className="update-bar"></div>
+                    </div>
+                    <button slot="ar-button" id="ar-button">
+                        View in your space
+                    </button>
             </model-viewer>
-        </div>
+        </html>
+        </>
     );
 };
-
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            'model-viewer': any;
-        }
-    }
-}
 
 export default ModelViewerComponent;
