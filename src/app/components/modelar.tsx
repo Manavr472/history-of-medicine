da6c6e112aae77f20f62746b159404687e559f46
Manavr472 <manavr472@gmail.com>
2025-02-26 21:18:22 +0530

    modelar

--- src/app/components/modelar.tsx
+++ src/app/components/modelar.tsx
@@ -4,7 +4,7 @@ import '@google/model-viewer';
 const ModelViewerComponent: React.FC = () => {
     return (
         <div>
-            <model-viewer src="3d-models/Glucks_First_Model_no_anime.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls tone-mapping="neutral" poster="poster.webp" shadow-intensity="1" style={{ width: '100%', height: '500px' }}>
+            <model-viewer src="3d-models/Glucks_First_Model_no_anime.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1" style={{ width: '100%', height: '500px' }}>
                 <div className="progress-bar hide" slot="progress-bar">
                     <div className="update-bar"></div>
                 </div>
