f18669f9bed7680e09712621f26ee16211e0f0f4
Manavr472 <manavr472@gmail.com>
2025-07-17 00:36:34 +0530

    remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 import React from 'react';
 import './globals.css';
 import Hero from './components/hero';
-import Landing from './components/landing';
-import SectionsPanel from './components/sections';
 
 export default function Home() {
   return (
